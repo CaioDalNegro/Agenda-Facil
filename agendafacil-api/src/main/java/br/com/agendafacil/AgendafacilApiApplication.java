febf6c6fc7c883030f555a468e5858fa08c829b3
CaioDalNegro <caiodalnegro@gmail.com>
2026-05-24 23:13:17 -0300

    first commit

---
+++ agendafacil-api/src/main/java/br/com/agendafacil/AgendafacilApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendafacilApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendafacilApiApplication.class, args);
+	}
+
+}
---
+++ agendafacil-api/src/test/java/br/com/agendafacil/AgendafacilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendafacilApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
