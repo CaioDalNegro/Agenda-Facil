733d4856eb137ea01b11e3b3540b138e2560b07d
CaioDalNegro <caiodalnegro@gmail.com>
2026-06-10 20:11:12 -0300

    NEW: Criando no migrate V2_create_table_barbers

---
+++ agendafacil-api/src/main/resources/db/migration/V2__create_table_barbers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE barbers (
+    -- ID único
+    id UUID PRIMARY KEY,
+
+    -- Nome do barbeiro
+    name VARCHAR(100),
+
+    -- Especialidade do barbeiro
+    specialty VARCHAR(100)
+);
